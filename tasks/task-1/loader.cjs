475861642cab5a26a1bb921e32d7cfb482c696a7
Yaroslav Snegir <snegirman@gmail.com>
2026-07-20 17:31:39 +0300

    task 1

--- tasks/task-1/loader.cjs
+++ tasks/task-1/loader.cjs
@@ -1,2 +1,10 @@
-const yaml = require("yaml");
 const fs = require("node:fs");
+const yaml = require("yaml");
+
+const parser = (module, filename) => {
+  module.exports = yaml.parse(fs.readFileSync(filename, 'utf8'));
+};
+
+require.extensions['.yml'] = parser;
+
+require.extensions['.yaml'] = parser;
--- tasks/task-1/loader.mjs
+++ tasks/task-1/loader.mjs
@@ -1,3 +1,18 @@
 import fs from "node:fs/promises";
 import { fileURLToPath } from "node:url";
 import yaml from "yaml";
+
+export async function load(url, context, nextLoad) {
+  if (!url.endsWith(".yml") && !url.endsWith(".yaml")) {
+    return nextLoad(url, context);
+  }
+
+  const source = await fs.readFile(fileURLToPath(url), "utf8");
+  const parsed = yaml.parse(source);
+
+  return {
+    format: "module",
+    shortCircuit: true,
+    source: `export default ${JSON.stringify(parsed)};`,
+  };
+}

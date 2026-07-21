c99ac6ced23a072001d2387936d6f35fa9999ec1
Yaroslav Snegir <snegirman@gmail.com>
2026-07-21 18:12:53 +0300

    task 2

--- .yarn/plugins/plugin-duplicates-list.cjs
+++ .yarn/plugins/plugin-duplicates-list.cjs
@@ -3,5 +3,38 @@ module.exports = {
   factory: (require) => {
     const { BaseCommand } = require(`@yarnpkg/cli`);
     const { Configuration, Project, structUtils } = require("@yarnpkg/core");
+
+    class DuplicatesList extends BaseCommand {
+      static paths = [['duplicates']];
+
+      async execute() {
+        const config = await Configuration.find(this.context.cwd, this.context.plugins);
+        const { project } = await Project.find(config, this.context.cwd);
+
+        await project.restoreInstallState();
+
+        const packageVersions = new Map();
+
+        for (const pkg of project.storedPackages.values()) {
+          if (!structUtils.isVirtualLocator(pkg)) {
+            const name = structUtils.stringifyIdent(pkg);
+            if (!packageVersions.has(name)) {
+              packageVersions.set(name, new Set());
+            }
+            packageVersions.get(name).add(pkg.version);
+          }
+        }
+
+        for (const [name, versions] of packageVersions) {
+          if (versions.size > 1) {
+            console.log(`${name}: ${Array.from(versions).join(' ')}`);
+          }
+        }
+      }
+    }
+
+    return {
+      commands: [DuplicatesList],
+    };
   },
 };

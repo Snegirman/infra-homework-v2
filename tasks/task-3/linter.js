46bc1eab33dd505c6cdd165e9898603d3fb04299
Yaroslav Snegir <snegirman@gmail.com>
2026-07-22 17:43:01 +0300

    task 3

--- tasks/task-3/linter.js
+++ tasks/task-3/linter.js
@@ -4,6 +4,37 @@ import { walk } from "zimmerframe";
 
 export function check(filePath) {
   const errors = [];
+  const file = fs.readFileSync(filePath).toString();
+  const parsed = parse(file, { ecmaVersion: 'latest', sourceType: 'module' });
+
+  const state = {
+    declarations: [],
+  };
+
+  walk(parsed, state, {
+    FunctionDeclaration(node, { next }) {
+      if (node.async) {
+        state.declarations.push(node.id.name);
+      }
+      next();
+    },
+  });
+
+  walk(parsed, state, {
+    IfStatement(node, { next }) {
+      if (node.test.type === "CallExpression") {
+        if (node.test.callee.type === "Identifier") {
+          if (state.declarations.includes(node.test.callee.name)) {
+            errors.push({
+              start: node.test.start,
+              end: node.test.end,
+            });
+          }
+        }
+      }
+      next();
+    },
+  });
 
   return errors;
 }

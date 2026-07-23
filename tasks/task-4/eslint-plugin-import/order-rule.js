26ba2084018538669968145ab9041b7fe9cdb146
Yaroslav Snegir <snegirman@gmail.com>
2026-07-23 17:33:33 +0300

    task 4

--- tasks/task-4/eslint-plugin-import/order-rule.js
+++ tasks/task-4/eslint-plugin-import/order-rule.js
@@ -1,3 +1,51 @@
+const getGroup = (value) => {
+  if (value.startsWith('./')) {
+    return 1;
+  }
+  if (value.startsWith('../')) {
+    return 2;
+  }
+  return 0;
+}
+
 export default {
-  
+  meta: {
+    fixable: "code",
+  },
+  create: (context) => {
+    const foundImports = [];
+
+    return {
+      ImportDeclaration(node) {
+        foundImports.push(node);
+      },
+      "Program:exit"() {
+        const imports = foundImports.map(x => x).sort((a, b) => {
+          const aGroup = getGroup(a.source.value);
+          const bGroup = getGroup(b.source.value);
+          if (aGroup === bGroup) {
+            return a.source.value.localeCompare(b.source.value);
+          }
+          return aGroup - bGroup;
+        });
+
+        const isSortedEquals = imports.every(
+          (sortedItem, index) => sortedItem === foundImports[index],
+        );
+
+        if (isSortedEquals) {
+          return;
+        }
+
+        context.report({
+          node: foundImports[0],
+          message: "Imports are not sorted by groups",
+          fix: (fixer) => {
+            const merged = imports.map(item => context.sourceCode.getText(item)).join("\n");
+            return fixer.replaceTextRange([foundImports[0].range[0], foundImports.at(-1).range[1]], merged);
+          }
+        })
+      },
+    }
+  }
 };

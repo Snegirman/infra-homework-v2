6ec06c575b8b013822b50585b521a2c8ef5c3b54
Yaroslav Snegir <snegirman@gmail.com>
2026-07-30 16:22:56 +0300

    task 7

--- tasks/task-7/lint-commit.js
+++ tasks/task-7/lint-commit.js
@@ -5,4 +5,12 @@ const TYPES = ["feat", "fix", "chore", "docs", "refactor", "test", "style"];
 // Read file with commit message
 const args = process.argv.slice(2);
 
-// Validate commit message
+const commitMsg = fs.readFileSync(args[0], "utf8");
+
+if (commitMsg.startsWith('Merge ')) {
+  console.log('VALID MERGE');
+} else if (TYPES.some(type => commitMsg.startsWith(`${type}:`))) {
+  console.log('VALID COMMIT');
+} else {
+  process.exit(1);
+}
